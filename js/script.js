a6f984e30b90d6340fba9808f61c5d07ebb286eb
Adrian Litewski <litewskidev@gmail.com>
2022-12-20 17:29:36 +0100

    add calculateTagClass function code

--- js/script.js
+++ js/script.js
@@ -88,7 +88,11 @@ function calculateTagsParams(tags){
 
 
 function calculateTagClass(count, params){
-  
+  const normalizedCount = count - params.min;
+  const normalizedMax = params.max - params.min;
+  const percentage = normalizedCount / normalizedMax;
+  const classNumber = Math.floor( percentage * (optCloudClassCount - 1) + 1 );
+  return optCloudClassPrefix + classNumber;
 }
 
 
@@ -139,8 +143,9 @@ function generateTags(){
   /* [DONE] START LOOP: for each tag in allTags: */
   for(let tag in allTags){
     /* [DONE] generate code of a link and add it to allTagsHTML */
-    allTagsHTML += '<li><a href="#tag-' + tag + '"><span>'+ tag +'</span><span> (' + allTags[tag] + ')</span></a></li>';
-    console.log(allTagsHTML);
+    const tagLinkHTML = '<li><a class="' + calculateTagClass(allTags[tag], tagsParams) + '" href="#tag-' + tag + '">'+ tag +'</a></li>';
+    console.log('tagLinkHTML: ', tagLinkHTML);
+    allTagsHTML += tagLinkHTML;
   /* [DONE] END LOOP: for each tag in allTags: */
   }
   /* [DONE] add html from allTagsHTML to tagList */
